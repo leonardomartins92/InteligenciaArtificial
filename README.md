737712f279fe77d58c1ae80dab25f494c096efde
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-01-13 14:51:45 -0300

    Atualizando Tecnicas

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# Estudos de AI e desenvolvimento de Projeto de Predição de Evasão Escolar
+# Estudos de AI 
 
-## Técnicas utilizadas:
+## Técnicas estudadas:
 
 - ANN com tuning
 - Classificador KNN
-- CNN 
-	-Dataset em: https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P16-Convolutional-Neural-Networks.zip
+- CNN: dataset em https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P16-Convolutional-Neural-Networks.zip
 
 
--- rnn.py
+++ rnn.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the training set
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+dataset_train = pd.read_csv('Dataset/CSV/Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
@@ -72,7 +72,7 @@ regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 # Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
-dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+dataset_test = pd.read_csv('Dataset/CSV/Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the predicted stock price of 2017
